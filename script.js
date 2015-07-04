036fc31c3f8f9c40690b7fe460ac3bd1ca81ce7d
Rob <robbcardenas@gmail.com>
2015-07-04 14:40:40 -0700

    slowly working through the variables and functions.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-$(function() {
+
+    // global variables
+    var $postContainer = $('#post-container');
+    var $author = $('#author');
+    var $postTitle = $('#post-title');
+    var $userPostContent = $('#post-content'); 
+
+    // variable that stores underscore template
+    var $postTemplate = _.template($('#post-template').html());
+
+
     // Constructor function
     function UserPost(author, postTitle, postContent) {
         this.author = author;
@@ -8,9 +18,15 @@ $(function() {
 
     // Testing data
     UserPost.allPosts = [
-        new UserPost('Rob','Kukulkan, the Ancient Serpent','During ancient times, the Mayans worshipped the wise Serpent');
-        new UserPost('Bere','The World\'s Most Venomous Snakes','This world is filled with tiny creatures but powerful enought to...');
+        new UserPost('Rob','Kukulkan, the Ancient Serpent','During ancient times the Mayans worshipped the wise Serpent'),
+        new UserPost('Bere','The World\'s Most Venomous Snakes','This world is filled with tiny creatures but powerful enought to...')
     ];
-    
 
-});
+    // Save function
+    UserPost.prototype.save = function() {
+        UserPost.allPosts.push(this);
+        console.log(UserPost.allPosts);
+    };
+    
+ 
+
