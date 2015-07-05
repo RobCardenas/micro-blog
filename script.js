9b1b14754e1201a6ca58dd2e29379885e955f65f
Rob <robbcardenas@gmail.com>
2015-07-04 18:02:56 -0700

    modified script and html files.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 $(function() {
 
     // global variables
-    var author = $('#author').val();
-    var postTitle = $('#post-title').val();
-    var userPostContent = $('#post-content').val();
-
     // variable that stores user posts
     var $postList = $("#post-list");
+
+    // variable that stores new submissions
+    var $newPostSub = $("#new-post-submission");
 
     // underscore template
     var postTemplate = _.template($('#post-template').html());
@@ -39,5 +38,29 @@ $(function() {
         $posts.attr('data-index', index);
         $postList.append($posts);
     });
+
+    // on click to submit our data
+    $newPostSub.on("submit", function(event) {
+    event.preventDefault();
+
+    // create new todo object from form data
+    var newAuthor = $('#author-name').val();
+    var newPostTitle = $('#new-post-title').val();
+    var newPostContent = $('#new-post-content').val();
+    var newPost = new UserPost(newAuthor,newPostTitle,newPostContent);
+
+    newPost.save();
+
+    // append our new todo to the page
+    var $posts = $(postTemplate(newPost));
+
+    $posts.attr('data-index', newPost.author, newPost.postTitle, newPost.userPostContent);
+    $postList.append($posts);
+
+    // reset the form
+    $newPostSub[0].reset();
+    $("#author-name").focus();
+
+  });
 
 });
