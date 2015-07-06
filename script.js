53e682db438fefe1f95af30782abf6c4e9366bf3
Rob <robbcardenas@gmail.com>
2015-07-05 23:44:31 -0700

    Added a post counter.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(function() {
 
-    
     // Tooltip to tell user they can hide the welcome post 
     $(function () {
     $('[data-toggle="tooltip"]').tooltip();
@@ -22,15 +21,16 @@ $(function() {
     // underscore template
     var postTemplate = _.template($('#post-template').html());
 
-    function UserPost(author,postTitle,userPost) {
+    function UserPost(author,location,postTitle,userPost) {
         this.author = author;
+        this.location = location;
         this.postTitle = postTitle;
         this.userPost = userPost;
     }
 
     UserPost.allPosts = [];
 
-    var testerPost = new UserPost("Rob", "Conquering Yosemite's Half Dome","Did you know that anyone who's in shape and willing to suffer a whole day can climb Yosemite's Half Dome? Well, you can. First of all, you'll need a permit which you can get at Recreation.gov and pay a reservation fee if you're able to score a permit. You will not be able to go up the cables without a permit, I repeat, the ranger will deny you access to the cables if you don't have a permit.  ");
+    var testerPost = new UserPost("Rob", "Killafornia", "Conquering Yosemite's Half Dome","Did you know that anyone who's in shape and willing to suffer a whole day can climb Yosemite's Half Dome? Well, you can. First of all, you'll need a permit which you can get at Recreation.gov and pay a reservation fee if you're able to score a permit. You will not be able to go up the cables without a permit, I repeat, the ranger will deny you access to the cables if you don't have a permit.  ");
 
     UserPost.prototype.save = function() {
         // saves new posts to array
@@ -57,24 +57,36 @@ $(function() {
 
         // create new post object from form data
         var newAuthor = $('#author-name').val();
+        var authorLocation = $('#author-location').val();
         var newPostTitle = $('#new-post-title').val();
         var newPostContent = $('#new-post-content').val();
-        var newPost = new UserPost(newAuthor,newPostTitle,newPostContent);
+        var newPost = new UserPost(newAuthor,authorLocation,newPostTitle,newPostContent);
 
-        if (newAuthor === '' || newPostTitle === '' || newPostContent === '' ) {
+        if (newAuthor === '' || authorLocation === '' || newPostTitle === '' || newPostContent === '' ) {
             alert("All fields required")
 
         } else {
             newPost.save();
             // append our new post to the page
             var $posts = $(postTemplate(newPost));
-            $posts.attr('data-index', newPost.author, newPost.postTitle, newPost.userPostContent);
+            $posts.attr('data-index', newPost.author, newPost.location, newPost.postTitle, newPost.userPostContent);
             $postList.append($posts);
         }
 
         // reset the form
         $newPostSub[0].reset();
         $("#author-name").focus();
+
+        // Post Counter
+        var $postCount = 0;
+        if ( UserPost.allPosts.length === [] ) {
+            $postCount;
+        } else {
+            $postCount += UserPost.allPosts.length;
+        }
+
+        var el = document.querySelector("#post-count");
+        el.innerText = "Post Count: " + $postCount;
 
   });
 
